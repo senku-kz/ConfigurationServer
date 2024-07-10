d0564ad5fc24cc7067aac68ff7db54f4f9ef80d1
Rustem <senku.kz@gmail.com>
2024-07-10 17:30:20 +0500

    add configuration for registration on consul

--- src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -1,10 +1,12 @@
 package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServiceApplication {
 
